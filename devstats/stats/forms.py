0d399bffa125be3299fe4f6f99e1db06e3645d08
Antonio Melé <antonio.mele@gmail.com>
2023-09-26 16:34:17 +0200

    multiple fixes

--- devstats/stats/forms.py
+++ devstats/stats/forms.py
@@ -1,10 +1,13 @@
+# django imports
 from django import forms
+
+# project imports
 from stats.models import Developer
 
 
 class DeveloperCreateForm(forms.ModelForm):
-
     def clean_username(self):
+        # return username in lowercase
         return self.cleaned_data['username'].lower()
 
     class Meta:
--- devstats/stats/models.py
+++ devstats/stats/models.py
@@ -1,7 +1,9 @@
-# django imports
-from django.db import models
-from django.conf import settings
+# 3rd. party imports
 from github import Github, GithubException
+
+# django imports
+from django.conf import settings
+from django.db import models
 
 
 class Developer(models.Model):
@@ -12,7 +14,7 @@ class Developer(models.Model):
     location = models.CharField(max_length=100,
                                 blank=True)
     company = models.CharField(max_length=100,
-                                blank=True)
+                               blank=True)
     avatar_url = models.URLField(blank=True)
     website_url = models.URLField(blank=True)
 
@@ -31,45 +33,56 @@ class Developer(models.Model):
         return self.username
 
     def _get_github_user(self):
-        if self._gh_user is None:
+        """
+        Retrieves a GitHub User
+        """
+        if not self._gh_user:
+            # github API auth
             gh = Github(settings.GITHUB_TOKEN,
                         per_page=settings.GITHUB_OBJECTS_PER_PAGE)
+            # retrieve github user profile
             self._gh_user = gh.get_user(self.username)
         return self._gh_user
 
     def update_profile_from_github(self):
+        """
+        Updates developer profile data from the GitHub API
+        """
         gh_user = self._get_github_user()
+        # update fields
         self.name = gh_user.name or ''
         self.location = gh_user.location or ''
-        self.company = gh_user.company or ''
         self.avatar_url = gh_user.avatar_url or ''
         self.website_url = gh_user.blog or ''
+        self.company = gh_user.company or ''
         self.total_followers = gh_user.followers
         self.total_following = gh_user.following
 
-        # save model to DB
+        # save model
         self.save()
 
     def update_repos_from_github(self):
+        """
+        Creates/updates user repositories from the Github API
+        """
+        # retrieve github user repos
         gh_repos = self._get_github_user().get_repos(type='owner')
         for gh_repo in gh_repos:
             # create repository if necessary
             repo, created = self.repos.get_or_create(name=gh_repo.name)
-            # update repo stats
+            # update repository stats
             repo.stargazers_count = gh_repo.stargazers_count
             repo.watchers_count = gh_repo.watchers_count
             repo.forks_count = gh_repo.forks_count
-
-            # save repo to DB
+            # save repository to DB
             repo.save()
 
 
 class Repo(models.Model):
-    name = models.CharField(max_length=250)
     developer = models.ForeignKey(Developer,
                                   related_name='repos',
                                   on_delete=models.CASCADE)
-
+    name = models.CharField(max_length=250)
     stargazers_count = models.PositiveIntegerField(default=0)
     watchers_count = models.PositiveIntegerField(default=0)
     forks_count = models.PositiveIntegerField(default=0)
